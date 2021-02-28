48841e8028716bcf906fdbae8e204226fe70bf50
claudiahalip <claudia4ro@yahoo.com>
2021-02-28 15:00:36 -0600

    post method on PostForm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
-
+import Posts from './components/Posts'
 import PostForm from './components/PostForm'
 
 function App() {
   return (
     <div className="App">
       <PostForm/>
+      <Posts/>
     </div>
   );
 }
--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,48 +1,96 @@
-import React, { Component } from 'react'
-import '../css/PostForm.css'
+import React, { Component } from 'react';
+import '../css/PostForm.css';
+import { DirectUpload } from 'activestorage';
 
-
-
-export default class PostForm extends Component {
-    state = {
-        title: "",
-        content: "",
-        featured_image: null
+class PostForm extends Component {
+    constructor(){
+        super()
+        this.state = {
+            title: '',
+            content: '',
+            featured_image: {}
+        }
     }
 
-    handleChange = (e)=>{
-        this.setState({
-           [e.target.name]: e.target.value
-        })
+    handleChange = (e) => {
+        
+        if(e.target.name === 'featured_image'){
+            this.setState({
+                [e.target.name]: e.target.files[0]
+            })
+        } else {
+            this.setState({
+                [e.target.name]: e.target.value
+            })
+        } 
     }
 
-    handleImage = (e)=>{
-        this.setState({
-            featured_image: e.target.files[0]
-        })
-    }
 
     handleSubmit = (e)=>{
         e.preventDefault()
         
+        console.log("merge????")
+
+        console.log(this.state)
+
+        let post = {
+            title: this.state.title,
+            content: this.state.content
+        }
+
+        fetch("http://localhost:3001/posts",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            }, 
+            body: JSON.stringify(post)
+        })
+        .then(response => response.json())
+        .then(response=> this.uploadFile(this.state.featured_image, response))
+
     }
+
+    uploadFile = (file, post)=>{
+       const upload = new DirectUpload(file, "http://localhost:3001/rails/active_storage/direct_uploads")
+       upload.create((error, blob) => {
+           if(error){
+               console.log(error)
+           }else{
+               console.log("there is no error")
+           }
+       })
+    }
+
 
     render() {
         return (
             <div className="postForm">
                 <p>Create your blog post:</p>
                 <div className="form">
-                <form onSubmit ={this.handleSubmit}>
+                <form onSubmit = {this.handleSubmit}>
                     <label>Title:</label>
                     <input 
                     className="select"
-                    type="text"></input>
+                    type="text"
+                    name='title'
+                    value={this.state.title}
+                    onChange={this.handleChange}
+                    ></input>
                     <label>Content:</label>
                     <input 
                     className="select"
-                    type="text"></input>
+                    type="textarea"
+                    name="content"
+                    value={this.state.content}
+                    onChange={this.handleChange}
+                    ></input>
                     
-                    <input type="file" className="chooseFile"></input>
+                    <input type="file" 
+                    className="chooseFile"
+                    name="featured_image"
+                    onChange={this.handleChange}
+                    ></input>
                     <input type = "submit" className="submit" ></input>
                 </form>
                 </div>
@@ -50,3 +98,5 @@ export default class PostForm extends Component {
         )
     }
 }
+
+export default PostForm
