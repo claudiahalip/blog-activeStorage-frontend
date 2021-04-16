e36990f9b4511742b6f822d132126646ea667298
claudiahalip <claudia4ro@yahoo.com>
2021-04-16 15:41:55 -0500

    filter feature

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -7,7 +7,8 @@ export default class Posts extends Component {
     constructor (){
         super()
         this.state = {
-            posts: []
+            posts: [],
+            searchTerm: ""
         }
     }
 
@@ -27,7 +28,17 @@ export default class Posts extends Component {
       })
     }
 
+    handleChange = (e) => {
+       this.setState({
+           searchTerm: e.target.value
+       })
+    }
+
     renderPosts = () =>{
+        const filterdPosts = this.state.posts.filter( post =>{
+            return post.title.toLowerCase().indexOf(this.state.searchTerm.toLowerCase()) !== -1
+        })
+        if(this.state.searchTerm == ""){
         return this.state.posts.map( post => { 
             console.log(post)
             return <Post  title = {post.title} 
@@ -35,12 +46,27 @@ export default class Posts extends Component {
             image = {post.image ? post.image : default_image   }
             key = {post.id}/>
         })
+        }
+        else{
+            return filterdPosts.map( post => { 
+                console.log(post)
+                return <Post  title = {post.title} 
+                content={post.content} 
+                image = {post.image ? post.image : default_image   }
+                key = {post.id}/>
+            })
+        }
     }
 
     render() {
+       
         return (
             <div>
                 <h1 className="postsTitle">Do you know?</h1>
+                <div>
+                    <label>Search:</label>
+                    <input type="text" onChange={this.handleChange} value = {this.state.searchTerm}></input>
+                </div>
                 {this.renderPosts()}
             </div>
         )
